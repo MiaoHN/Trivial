623798f0c4cbe25d0f14b690cf575cb7ffd2d667
MiaoHN <582418227@qq.com>
2022-11-03 15:51:34 +0800

    📦 leetcode: 940 题

---
+++ trivial/leetcode/source/q940.cpp
@@ -0,0 +1,21 @@
+#include "common.h"
+
+// 940. 不同的子序列Ⅱ
+// https://leetcode.cn/problems/distinct-subsequences-ii/
+
+// 给定一个字符串 s，计算 s 的不同非空子序列的个数
+
+// 思路：
+//  1. 如果新添加的符号
+
+class Solution {
+ public:
+  int distinctSubseqII(const string& s) { set<string> checkSet; }
+};
+
+int main(int argc, char const* argv[]) {
+  Solution solution;
+  string   str = "";
+  print(solution.distinctSubseqII(str));
+  return 0;
+}
