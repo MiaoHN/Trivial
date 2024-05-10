67de455c84b010e2193a30befa72896dbee8d4bf
MiaoHN <582418227@qq.com>
2024-05-10 22:45:34 +0800

    .

--- trivial/cprogram/7.7.c
+++ trivial/cprogram/7.7.c
@@ -1,45 +1,45 @@
-#include <stdio.h>
-#include <string.h>
-
-struct student {
+#include<stdio.h>
+ 
+struct Student
+{
     char stuID[10];
     char name[20];
     char sex;
     int age;
 };
-
-void insert_student(struct student* s) {
-    scanf("%s %s %c %d", s->stuID, s->name, &s->sex, &s->age);
-}
-
-void print_students(struct student* students, int count) {
-    if (count == 0) {
-        printf("null\n");
-    } else {
-        for (int i = 0; i < count; i++) {
-            printf("%s %s %c %d\n", students[i].stuID, students[i].name, students[i].sex, students[i].age);
-        }
-    }
-}
-
-int main() {
+ 
+ 
+int main()
+{
     int n;
-    scanf("%d", &n);
-
-    struct student students[100]; // 假设最多存储100个学生信息
-    int count = 0; // 记录当前已插入的学生数量
-
-    for (int i = 0; i < n; i++) {
-        char operation;
-        scanf(" %c", &operation);
-
-        if (operation == 'i') {
-            insert_student(&students[count]);
-            count++;
-        } else if (operation == 's') {
-            print_students(students, count);
+    scanf("%d",&n);
+    char action[n];
+    struct Student student[n];
+    getchar();
+    int j=0;
+    for(int k=0;k<n;k++)
+    {
+        scanf("%c",&action[k]);
+             
+            if(action[k]=='i')
+            {
+                scanf("%19s %c %d %9s",student[j].name,&student[j].sex,&student[j].age,student[j].stuID);
+                j++;
+            }
+            else if(action[k]=='s')
+            {
+                if(k==0)
+                printf("null");
+                else
+                for(int j=0;j<k;j++)
+                {
+                    printf("%s %s %c %d",student[j].stuID,student[j].name,student[j].sex,student[j].age);
+                    if(j!=k-1)
+                    printf("\n");
+                }
+            }
+            getchar();
         }
-    }
-
+     
     return 0;
 }
